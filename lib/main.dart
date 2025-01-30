da3c5821eef711e07accfed1031acde39de5d99b
Dayton Rumbold <45398901+Dayton-His-Glory-Alone@users.noreply.github.com>
2025-01-30 16:03:23 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'pages/home_page.dart';
 import 'splash_screen.dart';
 import 'pages/about_page.dart';
@@ -6,23 +7,57 @@ import 'pages/settings_page.dart';
 import 'pages/support_page.dart';
 import 'pages/verse_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isDarkMode = prefs.getBool('isDarkMode') ?? false;
+
+  runApp(MyApp(isDarkMode: isDarkMode));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  final bool isDarkMode;
+
+  MyApp({required this.isDarkMode});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late bool isDarkMode;
+
+  @override
+  void initState() {
+    super.initState();
+    isDarkMode = widget.isDarkMode;
+  }
+
+  void _toggleTheme(bool value) async {
+    setState(() {
+      isDarkMode = value;
+    });
+
+    // Save theme preference
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDarkMode', value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to your Verse Vault...',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
       initialRoute: '/',
       routes: {
         '/': (context) => HomePage(),
         '/about': (context) => AboutPage(),
-        '/settings': (context) => SettingsPage(),
+        '/settings': (context) => SettingsPage(
+              onThemeChanged: _toggleTheme,
+              isDarkMode: isDarkMode,
+            ),
         '/support': (context) => SupportPage(),
         '/verse': (context) => VersePage(),
       },
